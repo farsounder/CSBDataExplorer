5df1039ea9c82f45bd5b3c59f1877154b94b537a
Heath Henley <heath.henley@farsounder.com>
2024-05-15 15:58:00 -0400

    style updates and adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -5,38 +5,43 @@ import {
   ShareIcon,
 } from "@heroicons/react/24/outline";
 import Card from "@/app/_components/card";
+import SplashPageFooter from "./_components/splash-page-footer";
 
 const cards = [
   {
     title: "Map View",
-    description: "See your data, and data from other mariners on an interactive map viewer with a variety of base maps (ENCs, satellite, etc) and extra data layers.",
+    description:
+      "See your data, and data from other mariners on an interactive map viewer with a variety of base maps (ENCs, satellite, etc) and extra data layers.",
     icon: <GlobeAmericasIcon className="h-6 w-6 text-gray-800" />,
     link: "/dashboard/map",
   },
   {
     title: "Participating Organizations",
-    description: "A number of organizations including: Seakeepers Society, the IHO, NOAA, and many other industry partners collaborate to collect and share CSB data with Seabed 2030 and beyond.",
+    description:
+      "A number of organizations including: Seakeepers Society, the IHO, NOAA, and many other industry partners collaborate to collect and share CSB data with Seabed 2030 and beyond.",
     icon: <BuildingLibraryIcon className="h-6 w-6 text-gray-800" />,
-    link: "/",
+    link: "https://openbathymetry.cidco.ca/technology-partners",
   },
   {
-    title: "Learn More",
-    description: "This page should link to the new CSB website...",
+    title: "Learn More...",
+    description:
+      "Find out more about the IHO Data Center for Digital Bathymetry (DCDB), and the data that is available publicly.",
     icon: <ShareIcon className="h-6 w-6 text-gray-800" />,
-    link: "/",
+    link: "https://www.ncei.noaa.gov/iho-data-centre-digital-bathymetry",
   },
   {
     title: "Get Involved!",
-    description: "This page should link to the new CSB website...",
+    description:
+      "If you are FarSounder customer, you can share your data by installing the latest SonaSoft version and clicking 'Enable Cloud Sync' in settings. If not, check out the linked page to see if you're already working with a partner company.",
     icon: <QuestionMarkCircleIcon className="h-6 w-6 text-gray-800" />,
-    link: "/",
-  }
-]
+    link: "https://openbathymetry.cidco.ca/get-involved",
+  },
+];
 
 export default function Hero() {
   return (
-    <section className="w-full h-full  overflow-auto py-12 md:py-24 lg:py-32 bg-white">
-      <div className="container px-4 md:px-6">
+    <section className="w-full h-full flex flex-col  bg-white">
+      <div className="container px-4 md:px-6 h-full overflow-y-scroll pt-12 md:pt-24 lg:pt-32 pb-2">
         <div className="grid gap-6 items-center">
           <div className="flex flex-col justify-center space-y-8 text-center">
             <div className="space-y-2">
@@ -49,8 +54,8 @@ export default function Hero() {
               </h2>
               <p className="max-w-[600px] text-gray-600 md:text-xl mx-auto">
                 Explore the data collected and shared by{" "}
-                <span className="font-semibold">YOU</span> and other mariners 
-                users like you
+                <span className="font-semibold text-blue-600">YOU</span> and
+                other mariners users like you
               </p>
             </div>
             <div className="w-full max-w-full space-y-4 mx-auto">
@@ -63,6 +68,7 @@ export default function Hero() {
           </div>
         </div>
       </div>
+      <SplashPageFooter />
     </section>
   );
 }
