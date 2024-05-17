962134debfd20ad50c10847b3138bf26aa8e9821
Heath Henley <heath.henley@farsounder.com>
2024-05-17 11:44:24 -0400

    reorganize, clean out some extras

--- app/dashboard/map/_components/mapviewer.tsx
+++ app/dashboard/map/_components/mapviewer.tsx
@@ -14,11 +14,10 @@ import { useUser } from "@clerk/nextjs";
 
 import { UserData } from "@/lib/types";
 
+/* Constants, magic strings, types, etc */
 const NOAA_CSB_LAYER_NAME = "All CSB Data";
 const NOAA_S57_LAYER_NAME = "US S57";
 const OSM_BASE_LAYER_NAME = "OSM Base";
-const FS_BATHY_HS_LAYER_NAME = "FS Bathy (hs)";
-const FS_BATHY_LAYER_NAME = "FS Bathy";
 const USER_CSB_LAYER_NAME = "Your Data";
 
 const mapLayerIdToInfo = new Map<string, string>([
@@ -27,8 +26,6 @@ const mapLayerIdToInfo = new Map<string, string>([
     NOAA_S57_LAYER_NAME,
     "NOAA S57 ENC data as raster tiles, for US waters only.",
   ],
-  [FS_BATHY_HS_LAYER_NAME, "A PNG of hillshaded data bathymetry data."],
-  [FS_BATHY_LAYER_NAME, "A PNG of colorized bathymetry data."],
   [
     NOAA_CSB_LAYER_NAME,
     "Tracks from the DCDB Crowdsourced Bathymetry database.",
@@ -38,8 +35,6 @@ const mapLayerIdToInfo = new Map<string, string>([
 const mapLayerIdToDefaultVisibility = new Map<string, boolean>([
   [OSM_BASE_LAYER_NAME, true],
   [NOAA_S57_LAYER_NAME, false],
-  [FS_BATHY_HS_LAYER_NAME, false],
-  [FS_BATHY_LAYER_NAME, false],
   [NOAA_CSB_LAYER_NAME, false],
 ]);
 
@@ -54,51 +49,13 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
   bearing: 0,
 };
-
 type ViewState = typeof INITIAL_VIEW_STATE;
-
-const getTooltip = (info: PickingInfo): TooltipContent => {
-  return null;
-};
-
 type TypeToggleLayerHandler = (layerId: string) => void;
 
-const LayerVisibilityControl = ({
-  layers,
-  handler,
-}: {
-  layers: Layer[];
-  handler: TypeToggleLayerHandler;
-}) => {
-  return (
-    <div className="p-2 bg-white opacity-90 border rounded-lg">
-      <p className="border-b px-1 font-bold">Available Layers</p>
-      {layers.map((layer, i) => (
-        <div key={i} className="flex items-center gap-1">
-          <input
-            type="checkbox"
-            id={layer.id}
-            value={layer.id}
-            checked={layer.props.visible}
-            onChange={() => handler(layer.id)}
-          />
-          <label className="pl-2" htmlFor={layer.id}>
-            {mapLayerIdToInfo.has(layer.id) ? (
-              <span>{layer.id}</span>
-            ) : (
-              <span>{USER_CSB_LAYER_NAME}</span>
-            )}
-          </label>
-          <div className="group relative duration-300">
-            <InfoIcon />
-            <span className="absolute hidden group-hover:flex top-12 -left-64 -translate-y-full w-48 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm">
-              {mapLayerIdToInfo.get(layer.id) || CSB_INFO_TEXT}
-            </span>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+
+/* Helpers for components - getting default / specific layers, etc */
+const getTooltip = (info: PickingInfo): TooltipContent => {
+  return null;
 };
 
 const getLayerFilterString = (userData: UserData): string => {
@@ -149,6 +106,107 @@ const getCSBLayer = (userData: UserData): TileLayer | null => {
   });
   return layer;
 };
+
+const getDefaultLayers = (): Layer[] => {
+  return [
+    new TileLayer({
+      id: OSM_BASE_LAYER_NAME,
+      data: "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      minZoom: 0,
+      maxZoom: 19,
+      tileSize: 256,
+      visible: mapLayerIdToDefaultVisibility.get(OSM_BASE_LAYER_NAME) || false,
+      renderSubLayers: (props) => {
+        const {
+          boundingBox: [[west, south], [east, north]],
+        } = props.tile;
+        return new BitmapLayer(props, {
+          data: undefined,
+          image: props.data,
+          bounds: [west, south, east, north],
+        });
+      },
+    }),
+    new TileLayer({
+      id: NOAA_S57_LAYER_NAME,
+      visible: mapLayerIdToDefaultVisibility.get(NOAA_S57_LAYER_NAME) || false,
+      getTileData: (tile) => {
+        const bbox = tile.bbox as GeoBoundingBox;
+        const [west, south] = proj4("EPSG:4326", "EPSG:3857", [
+          bbox.west,
+          bbox.south,
+        ]);
+        const [east, north] = proj4("EPSG:4326", "EPSG:3857", [
+          bbox.east,
+          bbox.north,
+        ]);
+        const url =
+          "https://gis.charttools.noaa.gov/arcgis/rest/services/MCS/NOAAChartDisplay/MapServer/exts/MaritimeChartService/WMSServer?service=WMS&request=GetMap&layers=&styles=&format=image%2Fjpeg&transparent=false&version=1.1.1&&width=256&height=256&srs=EPSG%3A3857";
+        const layers = `layer=${"0,1,2,3,4,5,6,7"}`;
+        const bboxString = `bbox=${west},${south},${east},${north}`;
+        return url + "&" + layers + "&" + bboxString;
+      },
+      renderSubLayers: (props) => {
+        const {
+          boundingBox: [[west, south], [east, north]],
+        } = props.tile;
+        return new BitmapLayer(props, {
+          data: undefined,
+          image: props.data,
+          bounds: [west, south, east, north],
+        });
+      },
+    }),
+    new MVTLayer({
+      id: NOAA_CSB_LAYER_NAME,
+      visible: mapLayerIdToDefaultVisibility.get(NOAA_CSB_LAYER_NAME) || false,
+      data: "https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/csb_vector_tiles/VectorTileServer/tile/{z}/{y}/{x}",
+      lineWidthUnits: "pixels",
+      getLineWidth: 1,
+      getLineColor: [10, 10, 10],
+      pickable: false,
+      opacity: 0.5,
+    }),
+  ];
+};
+
+function LayerLegend({
+  layers,
+  handler,
+}: {
+  layers: Layer[];
+  handler: TypeToggleLayerHandler;
+}) {
+  return (
+    <div className="p-2 bg-white opacity-90 border rounded-lg">
+      <p className="border-b px-1 font-bold">Available Layers</p>
+      {layers.map((layer, i) => (
+        <div key={i} className="flex items-center gap-1">
+          <input
+            type="checkbox"
+            id={layer.id}
+            value={layer.id}
+            checked={layer.props.visible}
+            onChange={() => handler(layer.id)}
+          />
+          <label className="pl-2" htmlFor={layer.id}>
+            {mapLayerIdToInfo.has(layer.id) ? (
+              <span>{layer.id}</span>
+            ) : (
+              <span>{USER_CSB_LAYER_NAME}</span>
+            )}
+          </label>
+          <div className="group relative duration-300">
+            <InfoIcon />
+            <span className="absolute hidden group-hover:flex top-12 -left-64 -translate-y-full w-48 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm">
+              {mapLayerIdToInfo.get(layer.id) || CSB_INFO_TEXT}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function MapViewer() {
   const { isLoaded, isSignedIn, user } = useUser();
@@ -230,99 +288,8 @@ export default function MapViewer() {
         ref={legendRef}
         className="absolute invisible md:visible top-4 right-4 "
       >
-        <LayerVisibilityControl
-          layers={layers}
-          handler={handleToggleLayerVisibility}
-        />
+        <LayerLegend layers={layers} handler={handleToggleLayerVisibility} />
       </div>
     </div>
   );
 }
-
-// Moved into this function to make the main MapViewer component more readable
-const getDefaultLayers = (): Layer[] => {
-  return [
-    new TileLayer({
-      id: OSM_BASE_LAYER_NAME,
-      data: "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      minZoom: 0,
-      maxZoom: 19,
-      tileSize: 256,
-      visible: mapLayerIdToDefaultVisibility.get(OSM_BASE_LAYER_NAME) || false,
-      renderSubLayers: (props) => {
-        const {
-          boundingBox: [[west, south], [east, north]],
-        } = props.tile;
-        return new BitmapLayer(props, {
-          data: undefined,
-          image: props.data,
-          bounds: [west, south, east, north],
-        });
-      },
-    }),
-    new TileLayer({
-      id: NOAA_S57_LAYER_NAME,
-      visible: mapLayerIdToDefaultVisibility.get(NOAA_S57_LAYER_NAME) || false,
-      getTileData: (tile) => {
-        const bbox = tile.bbox as GeoBoundingBox;
-        const [west, south] = proj4("EPSG:4326", "EPSG:3857", [
-          bbox.west,
-          bbox.south,
-        ]);
-        const [east, north] = proj4("EPSG:4326", "EPSG:3857", [
-          bbox.east,
-          bbox.north,
-        ]);
-        const url =
-          "https://gis.charttools.noaa.gov/arcgis/rest/services/MCS/NOAAChartDisplay/MapServer/exts/MaritimeChartService/WMSServer?service=WMS&request=GetMap&layers=&styles=&format=image%2Fjpeg&transparent=false&version=1.1.1&&width=256&height=256&srs=EPSG%3A3857";
-        const layers = `layer=${"0,1,2,3,4,5,6,7"}`;
-        const bboxString = `bbox=${west},${south},${east},${north}`;
-        return url + "&" + layers + "&" + bboxString;
-      },
-      renderSubLayers: (props) => {
-        const {
-          boundingBox: [[west, south], [east, north]],
-        } = props.tile;
-        return new BitmapLayer(props, {
-          data: undefined,
-          image: props.data,
-          bounds: [west, south, east, north],
-        });
-      },
-    }),
-    new BitmapLayer({
-      id: FS_BATHY_HS_LAYER_NAME,
-      visible:
-        mapLayerIdToDefaultVisibility.get(FS_BATHY_HS_LAYER_NAME) || false,
-      bounds: [
-        [-71.457633569, 41.383995775],
-        [-71.457633569, 41.671457779],
-        [-71.288330215, 41.671457779],
-        [-71.288330215, 41.383995775],
-      ],
-      image: "/all_the_grids_rasterized_3000x5000hillshade_2.png",
-    }),
-    new BitmapLayer({
-      id: FS_BATHY_LAYER_NAME,
-      visible: mapLayerIdToDefaultVisibility.get(FS_BATHY_LAYER_NAME) || false,
-      bounds: [
-        [-71.457633569, 41.383995775],
-        [-71.457633569, 41.671457779],
-        [-71.288330215, 41.671457779],
-        [-71.288330215, 41.383995775],
-      ],
-      image: "/all_the_grids_rasterized_3000x5000color_3.png",
-      opacity: 0.7,
-    }),
-    new MVTLayer({
-      id: NOAA_CSB_LAYER_NAME,
-      visible: mapLayerIdToDefaultVisibility.get(NOAA_CSB_LAYER_NAME) || false,
-      data: "https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/csb_vector_tiles/VectorTileServer/tile/{z}/{y}/{x}",
-      lineWidthUnits: "pixels",
-      getLineWidth: 1,
-      getLineColor: [10, 10, 10],
-      pickable: false,
-      opacity: 0.5,
-    }),
-  ];
-};
