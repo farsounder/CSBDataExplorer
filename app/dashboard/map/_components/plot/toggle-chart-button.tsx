ee05e768506cb2ff03518534e7f15c88407e34a7
Heath Henley <heath.henley@farsounder.com>
2024-05-21 11:59:48 -0400

    update for weird landscape

--- app/dashboard/map/_components/plot/toggle-chart-button.tsx
+++ app/dashboard/map/_components/plot/toggle-chart-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChartBarSquareIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ToggleChartButton({
   children,
@@ -9,6 +9,16 @@ export default function ToggleChartButton({
   children: React.ReactNode;
 }) {
   const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerHeight < 600) {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
@@ -23,7 +33,7 @@ export default function ToggleChartButton({
         )}
       </button>
       {isVisible ? (
-        <div className="w-full sm:w-96 h-1/2 sm:h-80 sm:absolute sm:top-4 sm:left-4 z-10 bg-white rounded-lg p-1 shadow-md lg:w-2/5 lg:h-1/3">
+        <div className="w-full sm:w-96 h-1/2 sm:h-80 sm:absolute sm:top-4 sm:left-4 z-10 bg-white rounded-lg p-1 shadow-md lg:w-2/5 lg:h-1/3 min-h-[200px]">
           {children}{" "}
         </div>
       ) : null}
