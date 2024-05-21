346dfa9479f955824fc9b1a833aed00779310db2
Heath Henley <heath.henley@farsounder.com>
2024-05-21 17:30:32 -0400

    fixes for search params

--- app/api/og/[noaa_id]/route.tsx
+++ app/api/og/[noaa_id]/route.tsx
@@ -589,21 +589,41 @@ const FSLogo = () => (
   </svg>
 );
 
+const timeWindowValid = (
+  minDays: number,
+  maxDays: number,
+  time_window_days: number
+): boolean => {
+  return time_window_days >= minDays && time_window_days <= maxDays;
+};
+
 export async function GET(
   request: Request,
   {
     params,
-    searchParams,
   }: {
     params: { noaa_id: string };
-    searchParams?: { time_window_days: string };
   }
 ) {
-  const time_window_days = Number(searchParams?.time_window_days) || 30;
+  if (!params.noaa_id) {
+    return new Response("no id provided", { status: 404 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const time_window_days = Number(searchParams.get("time_window_days")) || 30;
+
   const data = await getPlatformData({
     noaa_id: params.noaa_id,
     time_window_days: time_window_days,
   });
+
+  if (!timeWindowValid(0, 365, time_window_days)) {
+    return new Response(
+      "Time window out of range, should be between 0 and 365 days",
+      { status: 404 }
+    );
+  }
+
   const noData = !data || data.length === 0;
   const provider = noData ? "No Data" : data[0].provider;
   const total_data_size = data.reduce((acc, d) => d.dataSize + acc, 0);
