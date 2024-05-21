557a394387577367ebf0e182dd23a109f2f801bc
Heath Henley <heath.henley@farsounder.com>
2024-05-21 10:29:49 -0400

    use custom suspence boundary so chart loads separately

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,9 +1,12 @@
-"use client"
+"use client";
+
+import ThreeDotsIcon from "@/components/icons/three-dots";
+
 export default function Loading() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <div className="w-24 h-24 border-4 border-blue-800 rounded-full animate-spin"></div>
+      <ThreeDotsIcon className="w-24 h-24" fill="#1E40AF" />
       <h1 className="text-2xl font-semibold text-gray-800 mt-4">Loading...</h1>
     </div>
-  )
+  );
 }
--- app/dashboard/map/page.tsx
+++ app/dashboard/map/page.tsx
@@ -1,7 +1,21 @@
 import MapViewer from "@/app/dashboard/map/_components/mapviewer";
 import PlotContainer from "@/app/dashboard/map/_components/plot/plot-container";
+import ThreeDotsIcon from "@/components/icons/three-dots";
+import { Suspense } from "react";
 
 const DEFAULT_PLOT_WINDOW_DAYS = 14;
+
+const LoadingMessage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-full px-8 text-center">
+      <ThreeDotsIcon className="w-16 h-16" fill="#FFFFF" />
+      <h3 className="text-lg text-gray-600">Loading contribution plot...</h3>
+      <p className="text-sm text-gray-500">
+        Data is being fetched, please wait a moment.
+      </p>
+    </div>
+  );
+};
 
 export default async function Page({
   searchParams,
@@ -12,7 +26,11 @@ export default async function Page({
     Number(searchParams?.time_window_days) || DEFAULT_PLOT_WINDOW_DAYS;
   return (
     <div className="flex flex-col p-0 m-0 h-full relative">
-      <PlotContainer time_window_days={time_window_days} />
+      <div className="w-full sm:w-96 h-1/2 sm:h-80 sm:absolute sm:top-4 sm:left-4 z-10 bg-white rounded-lg p-1 shadow-md lg:w-2/5 lg:h-1/3">
+        <Suspense fallback={<LoadingMessage />}>
+          <PlotContainer time_window_days={time_window_days} />
+        </Suspense>
+      </div>
       <MapViewer />
     </div>
   );
