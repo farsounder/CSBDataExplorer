fe40796ef2e1f49298ddc243cca73b2018678852
Heath Henley <heath.henley@farsounder.com>
2024-05-22 11:29:47 -0400

    Add share button gen image

--- app/dashboard/_components/user-ship-info.tsx
+++ app/dashboard/_components/user-ship-info.tsx
@@ -7,16 +7,42 @@ import {
 } from "../../../components/ui/tooltip";
 
 import { UserData } from "@/lib/types";
+import { ShareIcon } from "@heroicons/react/24/outline";
+import { ShipIcon } from "lucide-react";
+import Link from "next/link";
 
+function ShipCard({ userData }: { userData: UserData }) {
+  return (
+    <div className="flex flex-col items-center">
+      <ShipIcon className="w-4 h-4 sm:w-8 sm:h-8 text-blue-800" />
+      <div className="text-xs sm:text-sm text-gray-400">
+        {userData?.platform_nickname}
+      </div>
+    </div>
+  );
+}
+
+function ShareCard({ userData }: { userData: UserData }) {
+  const noaa_id = userData?.csbPlatform?.noaa_id;
+  if (!noaa_id) {
+    return null;
+  }
+  return (
+    <Link href={`/api/og/${noaa_id}`} target="_blank" >
+      <div className="flex flex-col items-center">
+        <ShareIcon className="w-4 h-4 sm:w-8 sm:h-8 text-blue-800" />
+        <div className="text-xs sm:text-sm text-gray-400">Share</div>
+      </div>
+    </Link>
+  );
+}
 export default function UserShipInfo({ userData }: { userData: UserData }) {
   return (
-    <div className="flex py-2 gap-4 justify-center items-center  sm:text-sm w-full z-50">
+    <div className="flex py-2 gap-2 justify-center items-center sm:text-sm w-full z-50">
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger>
-            {userData?.platform_nickname && (
-              <div>🚢{userData?.platform_nickname}</div>
-            )}
+            {userData?.platform_nickname && <ShipCard userData={userData} />}
           </TooltipTrigger>
           <TooltipContent>
             {userData?.csbPlatform?.platform && (
@@ -37,6 +63,16 @@ export default function UserShipInfo({ userData }: { userData: UserData }) {
                 Provider: {userData.csbPlatform.provider}
               </div>
             )}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            {userData?.platform_nickname && <ShareCard userData={userData} />}
+          </TooltipTrigger>
+          <TooltipContent>
+            Click to share the stats for this platform!
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
