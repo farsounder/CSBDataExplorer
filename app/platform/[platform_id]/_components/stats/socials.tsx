8c6eef42fd43bc7448bb7b3d6574493df16787e6
Heath Henley <heath.henley@farsounder.com>
2024-06-04 09:49:32 -0400

    change color

--- app/platform/[platform_id]/_components/stats/socials.tsx
+++ app/platform/[platform_id]/_components/stats/socials.tsx
@@ -7,13 +7,13 @@ export default function SocialButtons({ shareUrl }: { shareUrl: string }) {
   return (
     <div className="flex justify-center space-x-4">
       <FacebookShareButton url={shareUrl}>
-        <FacebookIcon className="w-8 h-8 text-blue-500" />
+        <FacebookIcon className="w-8 h-8 text-blue-800" />
       </FacebookShareButton>
       <LinkedinShareButton url={shareUrl}>
-        <LinkedinIcon className="w-8 h-8 text-blue-500" />
+        <LinkedinIcon className="w-8 h-8 text-blue-800" />
       </LinkedinShareButton>
       <Link href={shareUrl}>
-        <LinkIcon className="w-8 h-8 text-blue-500" />
+        <LinkIcon className="w-8 h-8 text-blue-800" />
       </Link>
     </div>
   );
