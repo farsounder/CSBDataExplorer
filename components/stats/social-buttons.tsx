24b6612773321d211f27282fe0a4a5eda13a0355
Heath Henley <heath.henley@farsounder.com>
2024-06-04 14:10:13 -0400

    add copy option, fix links

--- components/stats/social-buttons.tsx
+++ components/stats/social-buttons.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
-import { LinkIcon, LinkedinIcon } from "lucide-react";
+import { CopyIcon, LinkedinIcon } from "lucide-react";
+import { toast } from "../ui/use-toast";
 
 export default function SocialButtons({
   uniqueId,
@@ -10,16 +11,25 @@ export default function SocialButtons({
   timeWindowDays: number;
 }) {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-  const shareLink = `/share/${uniqueId}?time_window_days=${timeWindowDays}`;
-  const linkedInLink = `https://www.linkedin.com/sharing/share-offsite/?url=${baseUrl}/${encodeURIComponent(shareLink)}`;
+  const linkedIn = "https://www.linkedin.com/sharing/share-offsite/?url=";
+  const shareUrl = `/share/${uniqueId}?time_window_days=${timeWindowDays}`;
+  const fullShareUrl = `${baseUrl}${shareUrl}`;
+  const linkedInLink = `${linkedIn}${encodeURIComponent(fullShareUrl)}`;
   return (
     <div className="flex justify-center space-x-4">
       <Link href={linkedInLink}>
         <LinkedinIcon className="w-8 h-8 text-blue-800" />
       </Link>
-      <Link href={shareLink}>
-        <LinkIcon className="w-8 h-8 text-blue-800 hover:cursor-pointer" />
-      </Link>
+      <CopyIcon
+        className="w-8 h-8 text-blue-800 hover:cursor-pointer"
+        onClick={() => {
+          navigator.clipboard.writeText(fullShareUrl);
+          toast({
+            title: "Copied to clipboard",
+            description: "Share URL copied to clipboard",
+          });
+        }}
+      />
     </div>
   );
 }
